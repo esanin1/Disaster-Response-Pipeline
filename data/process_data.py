a8e9a4221f883f21d34f2614663127ff6814f2ec
esanin1 <esanin@its.jnj.com>
2023-03-17 03:56:53 +0000

    data and data processing ETL

---
+++ data/process_data.py
@@ -0,0 +1,89 @@
+import sys
+import pandas as pd
+import sqlite3
+import sqlalchemy
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    :param messages_filepath: filepath to the messages.csv file
+    :param categories_filepath: filepath to the  categories.csv categories file
+    :return: pandas dataframe of joint messages and categories data
+    '''
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, how="outer", on=["id"])
+    return (df)
+
+def clean_data(df):
+    '''
+    :param df: pandas dataframe to clean and transform
+    :return: cleaned pandas dataframe for ML processing
+    '''
+
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_names = row.apply(lambda x: x[:-2])
+    categories.columns = category_names
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    df=df.drop(['categories'], axis=1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+
+    # drop rows where "related" category is not in [0, 1]
+    df = df.drop(df[~df['related'].isin(['0', '1'])].index)
+    return (df)
+
+def save_data(df, database_filepath, table_name):
+    '''
+    :param df: paddas df to save in SQL db
+    :param database_filepath: filepath to SQL db
+    :param database_tablename: table name to save df to
+    :return: None
+
+    Note: Version 2.0.0 (January 26, 2023) of SQLAlchemy is not compatible with earlier versions of pandas.
+    -> latest pandas version 1.5.3 is required
+    Reference: https://stackoverflow.com/questions/75282511/df-to-table-throw-error-typeerror-init-got-multiple-values-for-argument
+    '''
+
+    engine = create_engine("sqlite:///" + database_filepath)
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+
+def main():
+    if len(sys.argv) == 5:
+
+        messages_filepath, categories_filepath, database_filepath, table_name = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {} TABLE: {}'.format(database_filepath,table_name))
+
+        save_data(df, database_filepath, table_name)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
